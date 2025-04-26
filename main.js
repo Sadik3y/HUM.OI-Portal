261bf8a612dcd2a79f76b49b9ab317d0bec8f43b
Sadik3y <nico.j92292@gmail.com>
2025-04-26 00:38:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ import bodyParser from 'body-parser';
 import path from 'path';
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 import 'dotenv/config';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 const app = express();
