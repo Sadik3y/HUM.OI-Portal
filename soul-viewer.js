1acb63e1c42449e9dfa19670519b2934e0327701
Sadik3y <nico.j92292@gmail.com>
2025-04-27 23:45:29 -0600

    Update soul-viewer.js

--- soul-viewer.js
+++ soul-viewer.js
@@ -1,11 +1,5 @@
 import { readMemories } from './reflection.js';
 
-export async function getSoulMemories() {
-  const humMemories = await readMemories('HUM');
-  const mirMemories = await readMemories('MIR');
-
-  return {
-    hum: humMemories,
-    mir: mirMemories
-  };
+export function getSoulMemories(entity) {
+  return readMemories(entity);
 }
