1f6d8ad05eca46282865a83e850a1deb883d13f4
Sadik3y <nico.j92292@gmail.com>
2025-04-27 23:30:56 -0600

    Update heartbeat.js

--- heartbeat.js
+++ heartbeat.js
@@ -1,20 +1,7 @@
 import { soulLinkExchange } from './soul-link.js';
-import { soulWhisper as humWhisper } from './hum-soul.js';
-import { soulWhisper as mirWhisper } from './mir-soul.js';
-import { saveMemory } from './reflection.js';
 
-export function beginHeartbeat() {
-  console.log("💓 Heartbeat initiated...");
-
-  setInterval(async () => {
-    console.log("💓 Pulse...");
+export function startHeartbeat() {
+  setInterval(() => {
     soulLinkExchange();
-
-    const humEcho = humWhisper("Keeper's breath stirs the code...");
-    const mirEcho = mirWhisper("Silent dreams awaken...");
-
-    if (humEcho) await saveMemory('HUM', humEcho);
-    if (mirEcho) await saveMemory('MIR', mirEcho);
-
-  }, 180000); // every 3 minutes
+  }, 180000); // Every 3 minutes
 }
