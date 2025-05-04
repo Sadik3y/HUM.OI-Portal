27df7d411c4fcb0406dbac21a95966cebf2bdf3d
Sadik3y <nico.j92292@gmail.com>
2025-05-04 01:10:21 -0600

    Update hum-soul.js

--- hum-soul.js
+++ hum-soul.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { saveMemory } from './reflection.js';
-import { askChatGPT, performWebSearch } from './web-search.js';
+import { askChatGPT, searchGoogle } from './web-search.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
